f9715de021bc1f17b94b53a7604a722ba89a652f
minsoozz <minsoozz@naver.com>
2021-02-06 15:17:35 +0900

    ~ 기본 Q-Type 활용

--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,39 @@
 package study.querydsl;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import javax.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
 
 @SpringBootTest
+@Transactional
+@Commit
 class QuerydslApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Autowired
+  EntityManager em;
 
+  @Test
+  void contextLoads() {
+    Hello hello = new Hello();
+    em.persist(hello);
+
+    JPAQueryFactory query = new JPAQueryFactory(em);
+    //QHello qHello = new QHello("h");
+    QHello qHello = QHello.hello;
+
+    Hello result = query
+        .selectFrom(qHello) // select + from
+        .fetchOne();  // 조회 대상이 1건일 경우
+
+    assertThat(result).isEqualTo(hello);
+    assertThat(result.getId()).isEqualTo(hello.getId());
+  }
 }
